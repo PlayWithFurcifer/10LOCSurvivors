b111e41fa13e1d8f6039fbdcb7ddf5440e2d4f9d
antoni-TL <antoni.kijjaroentorn@dxc.com>
2024-02-04 17:47:35 +0100

    fix for ver 4

--- Player.gd
+++ Player.gd
@@ -1,5 +1,5 @@
 extends Node2D
-func _physics_process(delta): position += Vector2(int(Input.is_key_pressed(KEY_D)) - int(Input.is_key_pressed(KEY_A)), int(Input.is_key_pressed(KEY_S)) - int(Input.is_key_pressed(KEY_W))) * $AnimatedSprite.speed_scale
-func shoot(): add_child(load("res://Bullet.tscn").instance())
-func spawn(): get_parent().add_child(load("res://Enemy.tscn").instance())
+func _physics_process(delta): position += Vector2(int(Input.is_key_pressed(KEY_D)) - int(Input.is_key_pressed(KEY_A)), int(Input.is_key_pressed(KEY_S)) - int(Input.is_key_pressed(KEY_W))) * $AnimatedSprite2D.speed_scale
+func shoot(): add_child(load("res://Bullet.tscn").instantiate())
+func spawn(): get_parent().add_child(load("res://Enemy.tscn").instantiate())
 func enemyContact(enemyHitbox): get_tree().reload_current_scene() #$DeathAnimationPlayer.play("Death")
